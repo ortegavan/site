0660130531e0708ab655312a8ec8ef916e582cd9
Vanessa Ortega <ortegavan@live.com>
2026-04-03 14:03:55 -0300

    feat: publica seção 'sobre'

--- src/app/components/home/home.ts
+++ src/app/components/home/home.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { Header } from '../header/header';
 import { Hero } from '../hero/hero';
+import { About } from '../about/about';
 
 @Component({
   selector: 'app-home',
-  imports: [Header, Hero],
+  imports: [Header, Hero, About],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
