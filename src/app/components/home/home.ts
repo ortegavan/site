8558cded41744faa1fc404b9e4cc822c9f70cdf7
Vanessa Ortega <ortegavan@live.com>
2026-04-03 16:25:04 -0300

    feat: publica seção de email

--- src/app/components/home/home.ts
+++ src/app/components/home/home.ts
@@ -4,10 +4,11 @@ import { Hero } from '../hero/hero';
 import { About } from '../about/about';
 import { Book } from '../book/book';
 import { Courses } from '../courses/courses';
+import { Email } from '../email/email';
 
 @Component({
   selector: 'app-home',
-  imports: [Header, Hero, About, Book, Courses],
+  imports: [Header, Hero, About, Book, Courses, Email],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
