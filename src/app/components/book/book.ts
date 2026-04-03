f77413554b29107dc85ed5453091aec413071d75
Vanessa Ortega <ortegavan@live.com>
2026-04-03 15:22:53 -0300

    feat: publica seção do livro

---
+++ src/app/components/book/book.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-book',
+  imports: [],
+  templateUrl: './book.html',
+  styleUrl: './book.scss',
+})
+export class Book {}
--- src/app/components/home/home.ts
+++ src/app/components/home/home.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { Header } from '../header/header';
 import { Hero } from '../hero/hero';
 import { About } from '../about/about';
+import { Book } from '../book/book';
 
 @Component({
   selector: 'app-home',
-  imports: [Header, Hero, About],
+  imports: [Header, Hero, About, Book],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
