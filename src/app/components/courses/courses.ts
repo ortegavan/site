e380d45c9c025ece7cdb72778c906cb39d734fe9
Vanessa Ortega <ortegavan@live.com>
2026-04-03 16:16:47 -0300

    feat: publica seção dos cursos

---
+++ src/app/components/courses/courses.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { Card } from '../../ui/card/card';
+
+@Component({
+  selector: 'app-courses',
+  imports: [Card],
+  templateUrl: './courses.html',
+  styleUrl: './courses.scss',
+})
+export class Courses {}
--- src/app/components/home/home.ts
+++ src/app/components/home/home.ts
@@ -3,10 +3,11 @@ import { Header } from '../header/header';
 import { Hero } from '../hero/hero';
 import { About } from '../about/about';
 import { Book } from '../book/book';
+import { Courses } from '../courses/courses';
 
 @Component({
   selector: 'app-home',
-  imports: [Header, Hero, About, Book],
+  imports: [Header, Hero, About, Book, Courses],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
